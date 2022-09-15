2164a487d844b3e5e9531e7b7dcfadbe8a438560
jay <jaewhan232@naver.com>
2022-09-15 21:59:57 +0900

    Feat - access토큰 재발급 api 구현

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from dj_rest_auth.jwt_auth import get_refresh_view
 
 from .views import *
 
@@ -6,4 +8,5 @@ urlpatterns = [
     path('signup/', UserSignupView.as_view()),
     path('login/', UserLoginView.as_view()),
     path('logout/', UserLogoutView.as_view()),
+    path('token/refresh/', get_refresh_view().as_view()),
 ]
