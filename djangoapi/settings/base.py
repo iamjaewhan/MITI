d0cd9d4969c56b3a30316b109fb3c421d5d93e77
jay <jaewhan232@naver.com>
2022-09-15 21:59:35 +0900

    Refact - 로그아웃 페이지 인증 절차 추가

--- djangoapi/settings/base.py
+++ djangoapi/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
+from datetime import timedelta
 
 import os, json
 
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate
 from rest_framework import status, views
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
@@ -39,12 +40,14 @@ class UserLoginView(views.APIView):
     
     
 class UserLogoutView(views.APIView):
+    permission_classes = [IsAuthenticated]
+    
     def post(self, request):
         try:
             refresh_token = request.data.get('refresh_token', None)
             token = RefreshToken(refresh_token)
             token.blacklist()
-
             return Response(status=status.HTTP_200_OK)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+        
