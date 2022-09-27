00b77e3962bb7b1e125690ce4fb9b02699fb9e16
jay <jaewhan232@naver.com>
2022-09-27 15:33:10 +0900

    Docs - 메소드 명세 작성

--- places/serializers.py
+++ places/serializers.py
@@ -8,5 +8,14 @@ class BasePlaceSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
     def create(self, validated_data):
+        """_summary_
+        유효성 검증을 마친 데이터를 사용하여 place 모델의 create() 호출
+
+        Args:
+            validated_data : 유효성 검증이 끝난 fields 의 dictionary
+
+        Returns:
+            생성된 place 객체
+        """
         instance = Place.objects.create(**validated_data)
         return instance
--- places/views.py
+++ places/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import views, status
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ValidationError
 
 
 from .models import *
@@ -11,17 +11,61 @@ from .serializers import *
 
 class PlaceListView(views.APIView):
     def get(self, request):
+        """_summary_
+        경기장 목록 조회 기능
+
+        Returns:
+            Response:
+                200 : 요청 정상 처리
+        """
         queryset = Place.objects.all()
         serializer = BasePlaceSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
+        """_summary_
+        경기장 등록 기능
+
+        Args:
+            request :
+                {
+                    "name": "경기장 이름",
+                    "address": "경기장 주소",
+                    "address_detail": "경기장 상세 주소" (optional),
+                    "contact": "경기장 연락처",
+                    "info": "장소 정보" (optional) ,
+                    "type": "장소 타입(실내,실외)"
+                }
+
+        Raises:
+            ValidationError: 유효하지 않은 입력값 입력받은 경우
+
+        Returns:
+            Response:
+                201 : 요청 정상 처리
+        """
         serializer = BasePlaceSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)class PlaceDetailView(views.APIView):
+        raise ValidationError("올바르지 않은 요청값입니다.")
+    
+
+class PlaceDetailView(views.APIView):
     def get(self, request, place_id):
+        """_summary_
+        경기장 상제 조회 기능
+
+        Args:
+            place_id (int): 조회할 경기장 id
+
+        Raises:
+            NotFound: 존재하지 않는 경기장 id일 경우
+
+        Returns:
+            Response:
+                200 : 요청 정상 처리
+        """
         queryset = Place.objects.filter(id=place_id)
         if queryset:
             serializer = BasePlaceSerializer(queryset[0])
