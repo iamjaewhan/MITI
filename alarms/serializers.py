976488d3f8b11ef1f57b271f679f62c17ebca59f
jay <jaewhan232@naver.com>
2022-10-19 14:31:54 +0900

    Feat -모델 변경에 따른  AlarmSerializer 수정

--- alarms/serializers.py
+++ alarms/serializers.py
@@ -3,7 +3,15 @@ from rest_framework import serializers
 from .models import Alarm
 
 
-class AlarmSerializer(serializers.ModelSerializer):    
+class AlarmSerializer(serializers.ModelSerializer):        
     class Meta:
         model = Alarm
-        fields = ['id', 'game', 'user']
+        fields = ['id', 'user', 'game', 'is_sent', 'is_checked', 'valid_until']
+        
+    def to_representation(self, obj):
+        representation = super().to_representation(obj)
+        game_id = representation.pop('game')
+        representation['game_id'] = game_id
+        representation['game_host'] = obj.game.host.email
+        representation['game_start_datetime'] = str(obj.game.start_datetime)
+        return representation
