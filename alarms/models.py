2f33255a1d71862f6e8f7bfa3df63470064db752
jay <jaewhan232@naver.com>
2022-10-19 14:30:57 +0900

    Feat - Alarm 모델 is_checked, valid_until 속성 추가 및 manager get_queryset 수정 is_checked : 사용자의 알람 확인 여부 valid_until : 알람의 유효 시간. 보통 경기의 시작 시간 get_queryset : 유효하고 확인하지 않은 알람 반환

--- alarms/models.py
+++ alarms/models.py
@@ -1,4 +1,5 @@
 from django.db import models, transaction
+from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 from games.models import Game
@@ -6,14 +7,16 @@ from games.models import Game
 # Create your models here.
 
 class AlarmManager(models.Manager):
-    def get_queryset(slef):
-        return super().get_queryset().filter(is_sent=False)
+    def get_queryset(self):
+        return super().get_queryset().filter(is_checked=False).filter(valid_until__gt=timezone.now())
 
 
 class Alarm(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     is_sent = models.BooleanField(null=False, default=False)
+    is_checked = models.BooleanField(null=False, default=False)
+    valid_until = models.DateTimeField(null=False, default=timezone.now)
     
     objects = AlarmManager()
     all_objects = models.Manager()
