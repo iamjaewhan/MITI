2d3bc20e8bea42fc8d8291834e18880ac8bfa64c
jay <jaewhan232@naver.com>
2022-11-25 16:04:12 +0900

    Refactor - 경기 참여 endpoint 수정 <game_id>/players/ -> <game_id>/participate

--- games/urls.py
+++ games/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', GameListView.as_view()),
     path('<int:game_id>/', GameDetailView.as_view()),
+    path('<int:game_id>/participate/', ParticipationView.as_view()),
     path('<int:game_id>/players/', PlayerListView.as_view()),
     path('<int:game_id>/players/<int:user_id>/', PlayerDetailView.as_view()),
 ]
