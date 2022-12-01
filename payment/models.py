33f5e4d242b86cdc4279bf7ec4f5fbf6b2a1d8e4
jay <jaewhan232@naver.com>
2022-12-01 10:57:17 +0900

    Refactor - 결제요청모델 필드 변경

--- payment/models.py
+++ payment/models.py
@@ -5,10 +5,12 @@ from games.models import Participation
 
 # Create your models here.
 class ParticipationPaymentRequest(models.Model):
-    tid = models.CharField(max_length=30, null=True, blank=True)
+    tid = models.CharField(max_length=30, unique=True)
+    item_code = models.CharField(max_length=30, null=True, blank=True)
     participation = models.OneToOneField(Participation, on_delete=models.PROTECT)
     quantity = models.IntegerField(default=1)
     total_amount = models.IntegerField(verbose_name=_("결제 총액"))
-    tax_free_amount = models.IntegerField(verbose_name=_("상품 비과세 금액"), blank=True, default=0)
+    tax_free_amount = models.IntegerField(verbose_name=_("상품 비과세 금액"), default=0, blank=True)
+    vat_amount = models.IntegerField(verbose_name=_("상품 부과세 금액"), default=0, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_request_at = models.DateTimeField(auto_now=True)
