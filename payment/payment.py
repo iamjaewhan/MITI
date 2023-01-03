3f72c9cc7cf14981dc795d69b94436029c718ed8
jay <jaewhan232@naver.com>
2023-01-03 17:48:31 +0900

    Refactor - 기존 KakaoApprovalDto 삭제

--- payment/payment.py
+++ payment/payment.py
@@ -25,37 +25,12 @@ class KakaoPayDto:
         for key in kwargs.keys():
             self.add_param(key, kwargs[key])
     
-class KakaoPayApprovalDto:
-    model = ParticipationPaymentRequest
-    required_param_keys = ('tid', 'partner_order_id', 'partner_user_id', 'pg_token')
-    params = {
-        'cid': getattr(settings, "KAKAO_CID"),
-    }
 
-    def __init__(self, obj : ParticipationPaymentRequest, pg_token=None):
-        assert isinstance(obj, KakaoPayApprovalDto.model), '해당 모델로는 요청을 생성할 수 없습니다.'
 
-        self.set_params(obj)
-        self.set_param('payment_request', obj.id)
-        self.set_param('pg_token', pg_token)
         
-    def set_params(self, obj : ParticipationPaymentRequest):
-        for key in self.required_param_keys:
-            self.params[key] = getattr(obj, key, None)
             
-    def set_param(self, key, value):
-        self.params[key] = value
             
-    def set_response(self, kwargs):
-        for key in kwargs.keys():
-            if key == "amount":
-                for k_key in kwargs[key].keys():
-                    self.params[k_key+"_"+key] = kwargs[key][k_key]
             else:
-                self.params[key] = kwargs.get(key, self.params.get(key, None))
-        
-    def get_params(self):
-        return self.params
         
         
 
