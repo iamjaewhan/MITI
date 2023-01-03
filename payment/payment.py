628facded274bc797bbc2384f70279cb3dd0ec28
jay <jaewhan232@naver.com>
2023-01-03 17:46:43 +0900

    Feat - 카카오 서버 요청시 사용할 Dto 클래스 구현 add_param() : request body 에 인자 추가 read_response() : 응답 body를 k-v 쌍으로 parameters에 저장

--- payment/payment.py
+++ payment/payment.py
@@ -1,44 +1,29 @@
 from django.conf import settings
 
 from games.models import Participation
-from payment.models import ParticipationPaymentRequest 
+from payment.models import ParticipationPaymentRequest, PaymentStatus, PaymentMethod
+
+from constants.custom_exceptions import (
+    RequestFailException,
+)
 
 
-class KakaoPayReadyDto:
-    model = ParticipationPaymentRequest
-    required_param_keys = ('partner_order_id', 'partner_user_id', 'quantity', 'total_amount', 'tax_free_amount', 'item_name')
-    params = {
-        'cid': getattr(settings, "KAKAO_CID"),
-        'approval_url': getattr(settings, "KAKAO_APPROVAL_URL"),
-        'cancel_url': getattr(settings, "KAKAO_FAIL_URL"),
-        'fail_url': getattr(settings, "KAKAO_CANCEL_URL")
+
+class KakaoPayDto:
+    parameters = {
+        'cid' : getattr(settings, "KAKAO_CID")
     }
     
-    def __init__(self, obj : ParticipationPaymentRequest):
-        assert isinstance(obj, KakaoPayReadyDto.model), '해당 모델로는 요청을 생성할 수 없습니다.'
-        
-        for key in self.required_param_keys:
-            self.params[key] = getattr(obj, key)
-            
-        self.set_urls(obj)
-    
-    def set_urls(self, obj:ParticipationPaymentRequest):
-        self.params['approval_url'] = self.params['approval_url']%(obj.participation.game.id, obj.participation.user.id)
-        self.params['cancel_url'] = self.params['cancel_url']%(obj.participation.game.id, obj.participation.user.id)
-        self.params['fail_url'] = self.params['fail_url']%(obj.participation.game.id, obj.participation.user.id)
-    
-    def read_response_data(self, **kwargs):
-        for key in kwargs.keys():
-            self.params[key] = kwargs[key]
-            
-    def add(self, key, value):
-        self.params[key] = value
-        
     def get_params(self):
-        return self.params
-
+        return self.parameters
     
             
+    def add_param(self, key, value):
+        self.parameters[key] = value
+        
+    def read_response(self, **kwargs):
+        for key in kwargs.keys():
+            self.add_param(key, kwargs[key])
     
 class KakaoPayApprovalDto:
     model = ParticipationPaymentRequest
