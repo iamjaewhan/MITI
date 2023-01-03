57817747d277ae24b601f59829da4f4f6267610c
jay <jaewhan232@naver.com>
2023-01-03 17:32:31 +0900

    Refactor - PaymentResult, ParticipationPaymentRequest 수정 PaymentResult - ParticipationPaymentRequest 참조 관계 수정 -> 1:1 item, payment_method_type, status 속성 enum속성으로 변경

--- payment/models.py
+++ payment/models.py
@@ -18,38 +18,40 @@ class PaymentMethod(models.TextChoices):
     CARD = 'CD'
     MONEY = 'MN'
     
+
+class PaymentResult(models.Model):    
+    aid = models.CharField(max_length=50, null=True, blank=True)
+    item_name = models.CharField(max_length=50)
+    quantity = models.IntegerField(default=1)
+
+    payment_method_type = models.CharField(
+        max_length=10, choices=PaymentMethod.choices, default=PaymentMethod.MONEY)
+    total_amount = models.IntegerField(
+        verbose_name=_("결제 총액"), null=True, blank=True)
+    tax_free_amount = models.IntegerField(
+        verbose_name=_("상품 비과세 금액"), null=True, blank=True)
+    vat_amount = models.IntegerField(
+        verbose_name=_("상품 부과세 금액"), null=True, blank=True)
+    status = models.CharField(
+        max_length=2, choices=PaymentStatus.choices, default=PaymentStatus.READY)
+    
+    approved_at = models.DateTimeField(null=True, blank=True)
+    canceled_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
 class ParticipationPaymentRequest(models.Model):
-    ##요청필수
-    item_name = models.CharField(max_length=30, choices=ITEM_NAME, default="PG")
+    item_name = models.CharField(max_length=10, choices=Item.choices, default=Item.PICKUP_GAME)
     partner_order_id = models.CharField(max_length=30)
     partner_user_id = models.CharField(max_length=30)
     quantity = models.IntegerField(default=1)
     total_amount = models.IntegerField(verbose_name=_("결제 총액"))
-    tax_free_amount = models.IntegerField(verbose_name=_("상품 비과세 금액"), default=0)
-    
+    tax_free_amount = models.IntegerField(verbose_name=_("상품 비과세 금액"), default=0, blank=True)
     vat_amount = models.IntegerField(verbose_name=_("상품 부과세 금액"), default=0, blank=True) 
-    #응답
     tid = models.CharField(max_length=30, null=True, blank=True)
     
     participation = models.OneToOneField(Participation, on_delete=models.PROTECT)
+    payment_result = models.OneToOneField(PaymentResult, on_delete=models.PROTECT)
     
     created_at = models.DateTimeField(auto_now_add=True)
     last_request_at = models.DateTimeField(auto_now=True)
-    
-
-
-class ParticipationPaymentResult(models.Model):    
-    # 응답
-    aid = models.CharField(max_length=50, null=True, blank=True)
-    item_name = models.CharField(max_length=50)
-    payment_request = models.ForeignKey(ParticipationPaymentRequest, on_delete=models.PROTECT)
-    quantity = models.IntegerField(default=1)
-
-    payment_method_type = models.CharField(default="MONEY", max_length=15, blank=True)
-    total_amount = models.IntegerField(verbose_name=_("결제 총액"), null=True, blank=True)
-    tax_free_amount = models.IntegerField(verbose_name=_("상품 비과세 금액"), null=True, blank=True)
-    vat_amount = models.IntegerField(verbose_name=_("상품 부과세 금액"), null=True, blank=True)
-    
-    approved_at = models.DateTimeField(null=True, blank=True)
-    canceled_at = models.DateTimeField(null=True, blank=True)
     
